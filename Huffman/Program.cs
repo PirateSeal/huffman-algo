73a13d96230b537d7e531132894824a31dfd07fa
PirateSeal <tcousin.pseal@gmail.com>
2020-10-09 16:48:28 +0200

    Auto stash before merge of "master" and "origin/master"

--- Huffman/Program.cs
+++ Huffman/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Linq;
 using System.Text;
 
@@ -12,16 +13,17 @@ namespace Huffman
         {
             string text = GetText();
             string bin = ToBinary(text);
+
             PrintBin(bin);
             GetTextLength(text);
-            GetTextLength(bin);
-
-            Console.ReadLine();
+            var dictionary = GetCharNbr(text);
+            Console.WriteLine(dictionary);
         }
 
         public static void PrintBin(string text)
         {
-            string bin = text.Aggregate("", (current, b) => current + b);
+            string bin = "";
+            foreach (char c in text) bin = bin + c;
             Console.WriteLine(bin);
             var path = "Output.txt";
             using (StreamWriter sw = File.AppendText(path))
@@ -44,11 +46,9 @@ namespace Huffman
 
         public static void GetTextLength(string text)
         {
-            var nbChar = 0;
-            foreach (char c in text)
-                nbChar++;
-
-            Console.WriteLine("text length is : {0}", nbChar);
+            var x = 0;
+            foreach (char c in text) x++;
+            Console.WriteLine("text length is : {0}", x);
         }
 
         public static String ToBinary(string text)
@@ -56,7 +56,22 @@ namespace Huffman
             var data = Encoding.ASCII.GetBytes(text);
 
             return string.Join("",
-                data.Select(byt => Convert.ToString(byt, 2).PadLeft(8, '0')));
+                data.Select(byt =>
+                    Convert.ToString(byt, 2)
+                        .PadLeft(8, '0')));
+        }
+
+        public static Dictionary<char, int> GetCharNbr(string text)
+        {
+            var occurrences = new Dictionary<char, int>();
+
+            foreach (char c in text)
+            {
+                if (!occurrences.ContainsKey(c)) occurrences.Add(c, 1);
+                else occurrences[c]++;
+            }
+            
+            return occurrences;
         }
     }
 }
