9b713a9dac62506d5f587ce114ffdd5e5d04ccd6
eltoredo <tanguy.e@hotmail.fr>
2020-10-09 16:43:22 +0200

    Refactor

--- Huffman/Program.cs
+++ Huffman/Program.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace Huffman
 {
@@ -9,46 +6,11 @@ namespace Huffman
     {
         public static void Main(string[] args)
         {
-            string text = GetText();
-            string bin = ToBinary(text);
-            PrintBin(bin);
-            GetTextLength(text);
+            string text = TextUtils.GetText();
+            string bin = TextUtils.ToBinary(text);
+            TextUtils.PrintBin(bin);
+            TextUtils.GetTextLength(text);
             Console.ReadLine();
-        }
-
-        public static void PrintBin(string text)
-        {
-            string bin = text.Aggregate("", (current, b) => current + b);
-            Console.WriteLine(bin);
-        }
-
-        public static string GetText()
-        {
-            Console.WriteLine("Enter text location");
-            string location = Console.ReadLine();
-            if (location != null && location.Contains(".txt"))
-            {
-                return System.IO.File.ReadAllText(location);
-            }
-
-            throw new Exception();
-        }
-
-        public static void GetTextLength(string text)
-        {
-            var nbChar = 0;
-            foreach (char c in text)
-                nbChar++;
-
-            Console.WriteLine("text length is : {0}", nbChar);
-        }
-
-        public static String ToBinary(string text)
-        {
-            var data = Encoding.ASCII.GetBytes(text);
-
-            return string.Join("",
-                data.Select(byt => Convert.ToString(byt, 2).PadLeft(8, '0')));
         }
     }
 }
