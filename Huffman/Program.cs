254d197ff6213ad99e52ae8caceef5e8de90ebc2
PirateSeal <tcousin.pseal@gmail.com>
2020-10-09 15:22:53 +0200

    oui.txt

--- Huffman/Program.cs
+++ Huffman/Program.cs
@@ -1,9 +1,37 @@
-﻿namespace Huffman
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace Huffman
 {
-  internal class Program
-  {
-    public static void Main(string[] args)
+    internal class Program
     {
+        public static void Main(string[] args)
+        {
+            var result = ToBinary(GetText());
+            string bin = result.Aggregate("", (current, b) => current + b);
+
+            Console.WriteLine(bin);
+        }
+
+        public static string GetText()
+        {
+            Console.WriteLine("Enter text location");
+            string location = Console.ReadLine();
+            if (location != null && location.Contains(".txt"))
+            {
+                return System.IO.File.ReadAllText(location);
+            }
+
+            throw new Exception();
+        }
+
+        public static String ToBinary(string text)
+        {
+            var data = Encoding.ASCII.GetBytes(text);
+
+            return string.Join(" ",
+                data.Select(byt => Convert.ToString(byt, 2).PadLeft(8, '0')));
+        }
     }
-  }
 }
